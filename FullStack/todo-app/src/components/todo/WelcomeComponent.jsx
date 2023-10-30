2decdc9f7403cad599412d14bac36f962ee0c147
Lilall5829 <lilall5829@gmail.com>
2023-10-30 13:46:58 -0400

    1030

--- FullStack/todo-app/src/components/todo/WelcomeComponent.jsx
+++ FullStack/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,26 @@
 import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 export default function WelcomeComponent() {
   // Get the username from URL
   const { username } = useParams();
+
+  function callHelloWorldRestApi() {
+    // How to call the rest apis in React: axios
+    axios
+      .get("http://localhost:8080/hello-world")
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log("cleanup")); // "then" means when rest API called successful, then do it
+  }
+
+  function successfulResponse(response) {
+    console.log(response);
+  }
+
+  function errorResponse(error) {
+    console.log(error);
+  }
+
   return (
     <div className="Welcome">
       <h1>Welcome {username}</h1>
@@ -9,6 +28,11 @@ export default function WelcomeComponent() {
         {/* Use Link tag (Link to),it should be imported above */}
         {/* Link tag helps your app render components that need re-rendering based on a specified route rather than refreshing the whole page like anchor tag! */}
         Manage your todos. <Link to="/todos">Start here</Link>
+      </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call hello world
+        </button>
       </div>
     </div>
   );
